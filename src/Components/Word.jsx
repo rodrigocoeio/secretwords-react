5bb4f49b48e32321c3498c564e111bb43ceb67c3
Rodrigo Coelho <rodrigocoeio@gmail.com>
2023-02-19 21:35:41 -0300

    Added text to speach when all letters are opened

--- src/Components/Word.jsx
+++ src/Components/Word.jsx
@@ -1,5 +1,6 @@
 import { connect } from "react-redux";
 import "./Word.css";
+import textToSpeach from "../store/textToSpeach";
 
 function getWordLetters(gameLetters, word) {
   let words = word.split(" ");
@@ -16,9 +17,13 @@ function getWordLetters(gameLetters, word) {
         letter: wordLetter,
         element:
           letter && letter.opened ? (
-            <span key={"letter-"+index} className="LetterBox Letter">{wordLetter}</span>
+            <span key={"letter-" + index} className="LetterBox Letter">
+              {wordLetter}
+            </span>
           ) : (
-            <span key={"cover-"+index} className="LetterBox">?</span>
+            <span key={"cover-" + index} className="LetterBox">
+              ?
+            </span>
           ),
       };
     });
@@ -29,11 +34,23 @@ function getWordLetters(gameLetters, word) {
 
 function Word({ gameLetters, word, allLettersOpened }) {
   const wordLetters = getWordLetters(gameLetters, word, allLettersOpened);
+  
+  let wordAudio = false;
+  async function playWord() {
+    if (allLettersOpened) {
+      wordAudio = wordAudio ? wordAudio : await textToSpeach(word);
+      playAudio(wordAudio);
+    }
+  }
 
   return (
     <div>
       {wordLetters.map((letters, index) => (
-        <div key={index} className={`Word ${allLettersOpened ? "Opened" : ""}`}>
+        <div
+          key={index}
+          onClick={playWord}
+          className={`Word ${allLettersOpened ? "Opened" : ""}`}
+        >
           {letters.map((letter) => letter.element)}
           <div className="clear"></div>
         </div>
@@ -46,5 +63,5 @@ export default connect((state) => ({
   gameLetters: state.letters,
   word: state.word,
   wordsIndex: state.wordsIndex,
-  allLettersOpened: state.allLettersOpened
+  allLettersOpened: state.allLettersOpened,
 }))(Word);
