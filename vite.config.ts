2d9d8eeae5c20cfdfa92efc3d67feecd8125f426
Rodrigo Coelho <rodrigocoeio@gmail.com>
2023-02-21 18:21:37 -0300

    Fixed aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path';
 
+const alias = {
+  '@': path.resolve(__dirname, './src/')+'\\',
+  '#': path.resolve(__dirname, './src/components/')+'\\',
+  '$': path.resolve(__dirname, './src/store/')+'\\',
+};
+
+console.log(alias);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,10 +18,6 @@ export default defineConfig({
     port: 93,
   },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '#': path.resolve(__dirname, './src/components'),
-      '$': path.resolve(__dirname, './src/store'),
-    }
+    alias: alias
   }
 })
