3036ba980f4adae71f6cca0b743271fe616a3b56
Rodrigo Coelho <rodrigocoeio@gmail.com>
2023-02-24 23:39:54 -0300

    Removed Wrapper, added fragment

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -3,15 +3,14 @@ import { FC } from "react";
 import Controls from "./Controls/Controls";
 import LettersControls from "./LettersControls/LettersControls";
 import Word from "./Word/Word";
-import Wrapper from "../Helpers/Wrapper";
 
 const Game: FC = () => {
   return (
-    <Wrapper>
+    <>
       <Controls />
       <LettersControls />
       <Word />
-    </Wrapper>
+    </>
   );
 };
 
